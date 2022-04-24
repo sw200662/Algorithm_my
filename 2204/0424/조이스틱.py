80751e0aea167cc320e8e6344e07ac026fd25921
leesw <sw200662@gmail.com>
2022-04-24 23:38:09 +0900

    0424

---
+++ 2204/0424/조이스틱.py
@@ -0,0 +1,19 @@
+name = "JEROEN"
+
+
+def solution(name):
+    answer = 0
+
+    move = len(name) - 1
+
+    for i, char in enumerate(name):
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        move = min([move, 2 * i + len(name) - next, i + 2 * (len(name) - next)])
+
+    answer += move
+    return answer
+solution(name)
